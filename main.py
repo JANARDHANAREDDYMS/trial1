42d27ddafd3a9a0bc955fbb9219bcbe6a801ca1c
Janardhana Reddy M S <76699907+janardhanareddyms@users.noreply.github.com>
2022-10-13 11:51:09 +0530

    first.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def first(x,y):
 
+    if x < y:
+        return y
+    else:
+        return x
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+x = int(input("Whats x:"))
+y = int(input("Whats y:"))
 
+maxval = first(x,y)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print("The max value is",maxval)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
